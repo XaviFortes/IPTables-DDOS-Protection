23e17bccc698353415806cbc57c86577548948ce
Xavi Fortes <53091080+XaviFortes@users.noreply.github.com>
2020-08-19 15:30:17 +0200

    Added the script and updater for further updates.

---
+++ updater.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+
+wget -O antiddos.sh 'https://raw.githubusercontent.com/XaviFortes/IPTables-DDOS-Protection/master/antiddos.sh'
+chmod +x antiddos.sh
+sudo yum install epel-release -y
+sudo yum install iptables-service -y
+clear
+echo "The antiddos rules will be installed in 5 seconds..."
+sleep 5
+clear
+bash antiddos.sh
+clear
+echo "The Rules were succesfully installed!!!"
+sleep 5
+exit
